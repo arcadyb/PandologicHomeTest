e5bdad0d506003ce4e2d24b6f664b3462802e43a
Arcady Barenboim <Arcady.Barenboim@ideo-digital.com>
2022-11-01 23:42:47 +0200

    router router view

---
+++ ClientApp/src/views/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div style="height:100%;width: 100%;">
+
+ HOMEPAGE
+    </div>
+</template>
+
+<script>
+
+import { computed, onMounted, onUpdated, onUnmounted } from 'vue'
+import { useStore, mapGetters } from 'vuex'
+
+export default {
+  name: 'HomePage',
+  components: {
+ 
+  },
+  props: {
+    //id: String
+
+  },
+  data () {
+    return {
+     // dtseries: {}
+    }
+  },
+  computed: {
+    // mix the getters into computed with object spread operator
+  },
+  setup (props) {
+    //const store = useStore()
+
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+
+    onMounted(() => {
+      //    store.dispatch('symbolmodule/GET_REQUEST', { symbol: 'usd' })
+    })
+    // const doughnutChart = computed(() => store.state.doughnutChart)
+
+    return {
+      // doughnutChart,
+      // selectedSymbol: computed(() => store.state.symbolmodule.selectedSymb)
+
+    }
+  }
+}
+</script>
